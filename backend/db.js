6e901a695b33b1fc0c0d1915767986c957582903
xavtry <sebastianishot68@gmail.com>
2025-10-25 23:25:33 -0400

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,16 @@
+// db.js
+const { Low } = require('lowdb');
+const { JSONFile } = require('lowdb/node');
+const path = require('path');
 
+const file = process.env.DB_FILE || path.join(__dirname, 'data', 'db.json');
+const adapter = new JSONFile(file);
+const db = new Low(adapter);
+
+async function init() {
+  await db.read();
+  db.data = db.data || { users: [], tracks: [], playlists: [], feed: [], analytics: {} };
+  await db.write();
+}
+
+module.exports = { db, init };

7a6c9befd6ec88ee04f4ca1c79dd47763a065640
xavtry <sebastianishot68@gmail.com>
2025-10-25 23:25:58 -0400

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,1 +1,30 @@
 
+// models/user.js
+const bcrypt = require('bcrypt');
+const { nanoid } = require('nanoid');
+const { db } = require('../db');
+
+async function createUser({ email, password, displayName }) {
+  const hash = await bcrypt.hash(password, 10);
+  const user = { id: 'u_' + nanoid(10), email, passwordHash: hash, displayName: displayName || 'New User', createdAt: Date.now(), following: [], likedTracks: [] };
+  db.data.users.push(user);
+  await db.write();
+  return sanitize(user);
+}
+
+async function findUserByEmail(email) {
+  return db.data.users.find(u => u.email === email);
+}
+
+async function findUserById(id) {
+  return db.data.users.find(u => u.id === id);
+}
+
+function sanitize(user) {
+  if (!user) return null;
+  const { passwordHash, ...rest } = user;
+  return rest;
+}
+
+module.exports = { createUser, findUserByEmail, findUserById, sanitize };
+
